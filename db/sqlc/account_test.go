2531aa71ecb98b93ec6ea9f26a2b5b45d40e988d
Piotr <gawronski.p@gmail.com>
2022-07-06 15:30:09 +0200

    Added unit test for Delete Account

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"github.com/spayder/simple-bank/util"
 	"github.com/stretchr/testify/require"
 	"testing"
@@ -57,4 +58,17 @@ func TestUpdateAccount(t *testing.T) {
 	require.NoError(t, err)
 	require.NotEmpty(t, updatedAccount)
 	require.Equal(t, updatedAccount.Balance, updateParams.Balance)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), account.ID)
+
+	require.NoError(t, err)
+
+	account2, err := testQueries.GetAccount(context.Background(), account.ID)
+
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, account2)
 }
